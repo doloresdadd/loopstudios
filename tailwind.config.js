a31d76a578ea68938b826f1e722ae9bbfd62327b
Bethanie Innes <doloresdadd@gmail.com>
2022-08-26 21:13:26 +1000

    Add Content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Josefin Sans", "Sans-Serif"],
+        alata: ["Alata"],
+      },
+      letterSpacing: {
+        widest: "0.3em",
+      },
+    },
   },
   plugins: [],
 };
